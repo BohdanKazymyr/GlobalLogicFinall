2f253c30d3ac470a6c55f565c0235d1a101a5785
Krab-master <kasumur1995@gmail.com>
2023-07-27 15:26:15 +0300

    first

--- StaticLib1/StaticLib1.cpp
+++ StaticLib1/StaticLib1.cpp
@@ -1,55 +1,74 @@
-﻿#include <iostream>
-#include <thread>
-#include <vector>
-#include <string>
+﻿#include "pch.h"
+
+#include <iostream>
 #include <filesystem>
+#include <vector>
+#include <thread>
+#include <future>
 #include <mutex>
-#include <atomic>
 
 namespace fs = std::filesystem;
-
+const int numberOfStream = 8;
 std::mutex printMutex;
-std::atomic<bool> stopSearching(false);
+bool has_directory_permission(const fs::path& dir) {
+    if (!fs::exists(dir)) {
+        return false; // File does not exist, so no permission
+    }
+
+    fs::file_status status = fs::status(dir);
+    if (fs::status_known(status) && fs::is_regular_file(status) && ((status.permissions() & fs::perms::owner_read) != fs::perms::none)) {
+        return true; // File has permission
+    }
+
+    return false; // File has no permission
+}
+std::vector<std::string> search_files_helper(const std::string& path, const std::string& query, std::atomic<bool>& stop_event, std::atomic<bool>& foundFlag) {
+    std::vector<std::string> results;
+    for (const auto& entry : fs::directory_iterator(path)) {
+        try {
+            if (stop_event) {
+                return results;
+            }
+          
+            const std::string& file = entry.path().filename().string();
+            const std::string& full_path = entry.path().string();
+            fs::file_status status = fs::status(full_path);
 
 
-void searchFile(const std::string& fileName, const fs::path& directory) {
-    for (const auto& entry : fs::directory_iterator(directory)) {
-        if (stopSearching) {
-            return;
+            if (fs::is_directory(entry) && !has_directory_permission(full_path)) {
+                std::vector<std::future<std::vector<std::string>>> futures;
+                
+                for (int i = 0; i < numberOfStream; ++i) {
+                    futures.push_back(std::async(std::launch::async, search_files_helper, full_path, query, std::ref(stop_event), std::ref(foundFlag)));
+                }
+                for (auto& future : futures) {
+                    std::vector<std::string> sub_results = future.get();
+                    results.insert(results.end(), sub_results.begin(), sub_results.end());
+                }
+            }
+            else {
+                
+                if (file.find(query) != std::string::npos && full_path.find("D:\\") == 0 && file.substr(file.size() - 4) == ".txt") {
+                    if (!foundFlag.exchange(true)) {
+                        std::lock_guard<std::mutex> lock(printMutex);
+                        std::cout << "Found file: " << full_path << std::endl;
+                        stop_event = true;
+                    }
+                    results.push_back(full_path);
+                }
+            }
         }
-        if (fs::is_directory(entry)) {
-            std::thread([](const std::string& fName, const fs::path& dir) {
-                searchFile(fName, dir);
-                }, fileName, entry.path()).detach();
-        }
-        else if (fs::is_regular_file(entry) && entry.path().filename() == fileName) {
-            std::lock_guard<std::mutex> lock(printMutex);
-            std::cout << "Found file: " << entry.path() << std::endl;
-            stopSearching = true;
-            return;
+        catch (const std::exception& ex) {
+           
+            std::cerr << "Exception caught: " << ex.what() << std::endl;
         }
     }
+    return results;
 }
 
 
-void initiateSearch(const std::string& fileNameToFind, const fs::path& rootDirectory) {
-
-    std::vector<std::thread> threads;
-    for (const auto& entry : fs::directory_iterator(rootDirectory)) {
-        if (threads.size() >= 8) {
-            break;
-        }
-        if (fs::is_directory(entry)) {
-            threads.emplace_back([](const std::string& fName, const fs::path& dir) {
-                searchFile(fName, dir);
-                }, fileNameToFind, entry.path());
-        }
-    }
-
-
-    for (auto& thread : threads) {
-        if (thread.joinable()) {
-            thread.join();
-        }
-    }
+std::vector<std::string> search_files(const std::string& path, const std::string& query) {
+    std::atomic<bool> stop_event(false);
+    std::atomic<bool> foundFlag(false);
+    return search_files_helper(path, query, stop_event, foundFlag);
 }
